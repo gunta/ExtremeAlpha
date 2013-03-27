53bcc15d9761bb723127eff146832c6e15892aa2
Gunther Brunner <gunta85@gmail>
2013-03-27 16:46:42 +0900

    * Overflow

--- js/image-format-comparison.js
+++ js/image-format-comparison.js
@@ -3,6 +3,10 @@
 
 
 	window.addEventListener('load', function () {
+		setTimeout(function () {
+			window.scrollTo(0, 1);
+		}, 0);
+
 		new FastClick(document.body);
 
 		var clockA = new utils.Clock();
@@ -13,7 +17,7 @@
 		queueA.addEventListener("complete", onCompleteQueueA);
 		$$('#btn-load-image-a').addEventListener('click', onClickButtonA);
 
-		function onCompleteQueueA () {
+		function onCompleteQueueA() {
 			var images = {
 				rgb: queueA.getResult('rgb'),
 				alpha: queueA.getResult('alpha')
@@ -26,7 +30,7 @@
 
 		}
 
-		function onClickButtonA () {
+		function onClickButtonA() {
 			clockA.getDeltaAsString();
 
 			var manifestA = [
@@ -37,29 +41,31 @@
 			queueA.loadManifest(manifestA);
 		}
 
+		drawBackground = function (ctx) {
+			var w = ctx.canvas.width, h = ctx.canvas.height, x, y;
+			var b = 16;
+			ctx.fillStyle = '#fff';
+			ctx.fillRect(0, 0, w, h);
+			ctx.fillStyle = '#ccc';
+			for (x = 0; x < w; x += b*2) {
+				for (y = 0; y < h; y += b*2) {
+					ctx.fillRect(x, y, b, b);
+					ctx.fillRect(x + b, y + b, b, b);
+				}
+			}
+		};
+
+		render = function (images) {
+			var canvas = $$('#canvas');
+			canvas.width = 2000;
+			canvas.height = 2000;
+			var ctx = canvas.getContext('2d');
+			var rgba = utils.toRgbaFromAlphaChannel(images.rgb, images.alpha);
+			drawBackground(ctx);
+			ctx.drawImage(rgba, 0, 0);
+		};
+
 	});
 
-	drawBackground = function (ctx) {
-		var w = ctx.canvas.width, h = ctx.canvas.height, x, y;
-		ctx.fillStyle = '#fff';
-		ctx.fillRect(0, 0, w, h);
-		ctx.fillStyle = '#ccc';
-		for (x = 0; x < w; x += 16) {
-			for (y = 0; y < h; y += 16) {
-				ctx.fillRect(x, y, 8, 8);
-				ctx.fillRect(x + 8, y + 8, 8, 8);
-			}
-		}
-	};
-
-	render = function (images) {
-		var canvas = $$('#canvas');
-		canvas.width = 2000;
-		canvas.height = 2000;
-		var ctx = canvas.getContext('2d');
-		var rgba = utils.toRgbaFromAlphaChannel(images.rgb, images.alpha);
-		drawBackground(ctx);
-		ctx.drawImage(rgba, 0, 0);
-	};
 
 }());
